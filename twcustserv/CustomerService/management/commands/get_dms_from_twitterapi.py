324d73b48352c7333d54fd8f5a2c09660cffe766
damian sastre <tagerr@gmail.com>
2015-02-25 02:52:48 -0300

    Update get_dms_from_twitterapi.py

--- twcustserv/CustomerService/management/commands/get_dms_from_twitterapi.py
+++ twcustserv/CustomerService/management/commands/get_dms_from_twitterapi.py
@@ -1,27 +1,21 @@
 from django.core.management.base import BaseCommand, CommandError
 from CustomerService.models import Thread, Message 
-
-
 #Base application to get DirectMessages from Twitter API
-
 import twitter
-#estos valores van el settings.
-api = twitter.Api(consumer_key='SJfHGWO9saCQAYvLLbTmX3CJQ',
+class Command(BaseCommand):
+			
+	def handle(self):
+		#estos valores van el settings.
+
+		api = twitter.Api(consumer_key='SJfHGWO9saCQAYvLLbTmX3CJQ',
 				  consumer_secret='j3ilbC5Nn7Pgug89wi1sQNpkAcKYcJ4U2ZdcJ18yrvlRdkZ6te', 
 				  access_token_key='30598040-kKdSKjnFWgS6h1L54ge3k4OKn2sTKYaWy4Qa3ARIn', 
 				  access_token_secret='c6iGAdcht4iACPJw7BBEMgOB3mnkIzNRSPlJx0ZdgxRrD')
 
-		
-
-#Management Command
-
-class Command(BaseCommand):
-			
-	def handle(self):
-	    self.screen_name = 'cgalceran'
+		self.screen_name = 'cgalceran'
 		#TwitterAPI jamas trae tus propios DirectMessages, trae solo incoming.
 		for msg in api.GetDirectMessages(self.screen_name):
-		    thread, created = Thread.objects.get_or_create(screen_name=msg.sender_screen_name, date_created=msg.created_at)
+			thread, created = Thread.objects.get_or_create(screen_name=msg.sender_screen_name, date_created=msg.created_at)
 			message, created = Message.objects.get_or_create(thread=threadmessage_id=msg.id, sender=msg.sender_id, message= msg.text)
 
 	
