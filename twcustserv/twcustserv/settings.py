b2e52c8905b261cc35093bffadf686fa1f21458a
Carlos <cargalceran@gmail.com>
2015-04-16 00:40:54 -0300

    Override Django Suit

--- twcustserv/twcustserv/settings.py
+++ twcustserv/twcustserv/settings.py
@@ -97,11 +97,12 @@ TEMPLATES_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )        
   
-
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 )
+
+print TEMPLATES_DIRS 
 
 
 # Static files (CSS, JavaScript, Images)
