8e27bf26844e46a461546656d5485b49bfaf768d
Andres Pozo <andpozo@gmail.com>
2016-06-17 20:56:16 -0300

    register in admin

--- twcustserv/customerservice/admin.py
+++ twcustserv/customerservice/admin.py
@@ -3,7 +3,7 @@ from django.conf import settings
 # Register your models here.
 from django.forms import TextInput, Textarea
 from django.db import models
-from customerservice.models import Thread, Message , Bulletin, Topic, TopicField, TopicForm, Enquiry, EnquiryResponse
+from customerservice.models import Contact, Thread, Message , Bulletin, Topic, TopicField, TopicForm, Enquiry, EnquiryResponse
 from django.conf import settings
 from django.core.mail import EmailMessage
 from django.http import HttpResponseRedirect
@@ -38,97 +38,12 @@ class MessageStackedInline(admin.StackedInline):
     suit_classes = 'suit-tab suit-tab-msgs'
 
 
-class ThreadAdmin(admin.ModelAdmin):
-    inlines = [MessageStackedInline, ]
-    list_filter = ('status', 'assigned_to' )
-    readonly_fields = ['screen_name', 'user_id', 'date_created']
-    list_display = ('screen_name', 'date_created')
-    search_fields = ("screen_name", "user_id", "status", "thread_messages__message")
-
-    
-    def get_readonly_fields(self, request, obj=None):
-        try:
-            user_group = request.user.groups.all()[0]
-        except:
-            return self.readonly_fields + ['assigned_to']
-        if 'Administrador' == user_group.name or 'Supervisor' in user_group.name:
-            return self.readonly_fields
-        return self.readonly_fields + ['assigned_to']
-
-    def suit_row_attributes(self, Thread, request):
-        css_class = {
-            settings.CLOSED: 'error',
-            settings.PENDING: 'warning',
-            settings.OPEN: 'success'}.get(Thread.status)
-        if css_class:
-            return {'class': css_class, 'data': Thread}
-
-    def has_add_permission(self, request):
-        return False
-
-    def save_formset(self, request, form, formset, change):
-        api = twitter.Api(consumer_key=settings.CONSUMER_KEY, consumer_secret=settings.CONSUMER_SECRET,
-                          access_token_key=settings.ACCESS_TOKEN_KEY, access_token_secret=settings.ACCESS_TOKEN_SECRET)
-        formset.save()
-        if change:
-            for f in formset.forms:
-                if f.changed_data:
-                    obj = f.instance
-                    obj.user = request.user
-                    signature = ' - ' + ''.join(request.user.first_name[0]+request.user.last_name[0])
-                    for message in split(obj.message, signature):
-                        try:
-                            own_msg = api.PostDirectMessage(message, obj.thread.user_id)
-                            print(message)
-                        except:
-                            pass
-                    if own_msg:
-                        obj.message_id = str(own_msg.id)
-                        obj.creator = True
-                    obj.save()
-
-    fieldsets = [
-        (None, {
-            'classes': ('suit-tab', 'suit-tab-general',),
-            'fields': ['screen_name', 'user_id', 'date_created', 'status', 'assigned_to', 'email']
-        }),
-
-    ]
-    suit_form_tabs = (('general', 'General'), ('msgs', 'Mensajes') , ('info', 'Info de la cuenta'))
-
-    suit_form_includes = (
-        ('enq_user_info.html', '', 'info'),
-    )
-
-
-class BulletinAdmin(admin.ModelAdmin):
-    list_display = ('user', 'text')
-    fields = ('text', 'important')
-
-    def save_model(self, request, obj, form, change):
-        if not obj.user:
-            obj.user = request.user
-        obj.save()
-
-class TopicFieldInLine(admin.StackedInline):
-    model = TopicField
-    extra = 1
-
-class TopicFormAdmin(admin.ModelAdmin):
-    inlines = [TopicFieldInLine,]
-
-class TopicInline(admin.StackedInline):
-    model = Topic
-    extra = 1
-
-class TopicAdmin(admin.ModelAdmin):
-    inlines = [TopicInline]
-
 class EnquiryMessageInline(admin.StackedInline):
     model = EnquiryResponse
     exclude = ['creator', 'sender', 'user', 'message_id']
     extra = 1
     suit_classes = 'suit-tab suit-tab-msgs'
+
 
 def bulk_send(modeladmin, request, queryset):
     ids = [str(obj.pk) for obj in queryset]
@@ -136,17 +51,98 @@ def bulk_send(modeladmin, request, queryset):
 
 bulk_send.short_description = 'Bulk Send'
 
-from django.views.generic.edit import FormView
-from .forms import BulkSendEnquiryForm
 
-class BulkSend(FormView):
-    template_name = 'bulk_send_enquiry.html'
-    form_class = BulkSendEnquiryForm
-    success_url = '/admin/customerservice/enquiry/'
+class ContactAdmin(admin.ModelAdmin):
 
-    def form_valid(self, form):
-        form.send(self.kwargs['ids'])
-        return HttpResponseRedirect(self.success_url)
+    inlines = [MessageStackedInline, EnquiryMessageInline]
+    actions = [bulk_send]
+    list_filter = ['status', 'assigned_to', 'type']
+    readonly_fields = ['screen_name', 'user_id', 'date_created', 'topic',
+                       'date', 'email', 'file', 'enquiry']
+    list_display = ['screen_name', 'created', 'topic', 'status']
+    search_fields = ["screen_name", "user_id", "status", "thread_messages__message"]
+
+    fieldsets = [
+        (None,{
+            'classes': ('suit-tab', 'suit-tab-general'),
+            'fields': ['screen_name', 'user_id', 'created', 'status',
+                       'assigned_to', 'topic', 'enquiry', 'email', 'file']
+        }),
+
+    ]
+
+    suit_form_tabs = (
+        ('general', 'General'),
+        ('msgs', 'Mensajes'),
+        ('info', 'Info de la cuenta')
+    )
+
+    suit_form_includes = (
+        ('enq_user_info.html', '', 'info'),
+    )
+
+
+    def suit_row_attributes(self, obj, request):
+        css_class = {
+            Contact.CLOSED: 'error',
+            Contact.PENDING: 'warning',
+            Contact.OPEN: 'success'}.get(obj.status)
+        if css_class:
+            return {'class': css_class, 'data': obj}
+
+    def get_readonly_fields(self, request, obj=None):
+        # only apply if is Thread?
+        try:
+            user_groups = request.user.groups.values_list('name', flat=True)
+            if 'Administrador' in user_groups or 'Supervisor' in user_groups:
+                return self.readonly_fields
+        except:
+            pass
+        return self.readonly_fields + ['assigned_to']
+
+    def has_add_permission(self, request):
+        return False
+
+    def save_formset(self, request, form, formset, change):
+        # TODO: Performance Issue/User experience
+        # Many features implementend in following lines could be replaced by
+        # async tasks in celery
+        formset.save()
+        if change:
+            for f in formset.forms:
+                if f.changed_data:
+                    obj = f.instance
+                    obj.user = request.user
+                    signature = ' - ' + ''.join(request.user.first_name[0]+request.user.last_name[0])
+                    if obj.type == Contact.THREAD:
+                        api = twitter.Api(
+                            consumer_key=settings.CONSUMER_KEY,
+                            consumer_secret=settings.CONSUMER_SECRET,
+                            access_token_key=settings.ACCESS_TOKEN_KEY,
+                            access_token_secret=settings.ACCESS_TOKEN_SECRET
+                        )
+                        for message in split(obj.message, signature):
+                            try:
+                                own_msg = api.PostDirectMessage(message, obj.thread.user_id)
+                                print(message)
+                            except:
+                                pass
+                        if own_msg:
+                            obj.message_id = str(own_msg.id)
+                            obj.creator = True
+                    else if obj.type == Contact.ENQUIRY:
+                        try:
+                            message_id = email.utils.make_msgid()
+                            em = EmailMessage('Respuesta Atencion al cliente', obj.message,
+                                                'ayuda@ticketek.com.ar', [obj.thread.email],
+                                                headers={'Message-ID': message_id})
+                            em.send()
+                        except:
+                            pass
+                        else:
+                            obj.message_id = message_id
+                    obj.save()
+
 
 class EnquiryAdmin(admin.ModelAdmin):
     list_display = ('topic', 'date', 'status')
@@ -204,6 +200,103 @@ class EnquiryAdmin(admin.ModelAdmin):
     suit_form_includes = (
         ('enq_user_info.html', '', 'info'),
     )
+
+class ThreadAdmin(admin.ModelAdmin):
+    inlines = [MessageStackedInline, ]
+    list_filter = ('status', 'assigned_to' )
+    readonly_fields = ['screen_name', 'user_id', 'date_created']
+    list_display = ('screen_name', 'date_created')
+    search_fields = ("screen_name", "user_id", "status", "thread_messages__message")
+
+    
+    def get_readonly_fields(self, request, obj=None):
+        try:
+            user_group = request.user.groups.all()[0]
+        except:
+            return self.readonly_fields + ['assigned_to']
+        if 'Administrador' == user_group.name or 'Supervisor' in user_group.name:
+            return self.readonly_fields
+        return self.readonly_fields + ['assigned_to']
+
+    def suit_row_attributes(self, Thread, request):
+        css_class = {
+            settings.CLOSED: 'error',
+            settings.PENDING: 'warning',
+            settings.OPEN: 'success'}.get(Thread.status)
+        if css_class:
+            return {'class': css_class, 'data': Thread}
+
+    def has_add_permission(self, request):
+        return False
+
+    def save_formset(self, request, form, formset, change):
+        api = twitter.Api(consumer_key=settings.CONSUMER_KEY, consumer_secret=settings.CONSUMER_SECRET,
+                          access_token_key=settings.ACCESS_TOKEN_KEY, access_token_secret=settings.ACCESS_TOKEN_SECRET)
+        formset.save()
+        if change:
+            for f in formset.forms:
+                if f.changed_data:
+                    obj = f.instance
+                    obj.user = request.user
+                    signature = ' - ' + ''.join(request.user.first_name[0]+request.user.last_name[0])
+                    for message in split(obj.message, signature):
+                        try:
+                            own_msg = api.PostDirectMessage(message, obj.thread.user_id)
+                            print(message)
+                        except:
+                            pass
+                    if own_msg:
+                        obj.message_id = str(own_msg.id)
+                        obj.creator = True
+                    obj.save()
+
+    fieldsets = [
+        (None, {
+            'classes': ('suit-tab', 'suit-tab-general',),
+            'fields': ['screen_name', 'user_id', 'date_created', 'status', 'assigned_to', 'email']
+        }),
+    ]
+    suit_form_tabs = (('general', 'General'), ('msgs', 'Mensajes') , ('info', 'Info de la cuenta'))
+
+    suit_form_includes = (
+        ('enq_user_info.html', '', 'info'),
+    )
+
+
+class BulletinAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text')
+    fields = ('text', 'important')
+
+    def save_model(self, request, obj, form, change):
+        if not obj.user:
+            obj.user = request.user
+        obj.save()
+
+class TopicFieldInLine(admin.StackedInline):
+    model = TopicField
+    extra = 1
+
+class TopicFormAdmin(admin.ModelAdmin):
+    inlines = [TopicFieldInLine,]
+
+class TopicInline(admin.StackedInline):
+    model = Topic
+    extra = 1
+
+class TopicAdmin(admin.ModelAdmin):
+    inlines = [TopicInline]
+
+from django.views.generic.edit import FormView
+from .forms import BulkSendEnquiryForm
+
+class BulkSend(FormView):
+    template_name = 'bulk_send_enquiry.html'
+    form_class = BulkSendEnquiryForm
+    success_url = '/admin/customerservice/enquiry/'
+
+    def form_valid(self, form):
+        form.send(self.kwargs['ids'])
+        return HttpResponseRedirect(self.success_url)
 
 
 class TransactionInline(admin.StackedInline):
@@ -305,6 +398,7 @@ admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
 admin.site.register(Bulletin, BulletinAdmin)
+admin.site.register(Contact, ContactAdmin)
 admin.site.register(Thread, ThreadAdmin)
 admin.site.register(Topic, TopicAdmin)
 admin.site.register(TopicForm, TopicFormAdmin)
